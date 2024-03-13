6981f5aa8e5055ff62bc8fb3bd60aadc8e185af8
ericz02 <ezheng0720@gmail.com>
2024-03-12 21:31:38 -0400

    feat: sets up stocks & orders controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_10_002722) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_13_011805) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,24 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_10_002722) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "admin_orders", force: :cascade do |t|
+    t.string "customer_email"
+    t.boolean "fulfilled"
+    t.integer "total"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "admin_stocks", force: :cascade do |t|
+    t.string "size"
+    t.integer "amount"
+    t.integer "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_admin_stocks_on_product_id"
   end
 
   create_table "admins", force: :cascade do |t|
@@ -71,5 +89,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_10_002722) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "admin_stocks", "products"
   add_foreign_key "products", "categories"
 end
