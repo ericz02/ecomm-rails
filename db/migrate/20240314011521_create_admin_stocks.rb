f85274f52a61b9207949c622ced3637326f4214e
ericz02 <ezheng0720@gmail.com>
2024-03-13 21:44:54 -0400

    bug: fixed issue of undefined method admin_stocks

--- db/migrate/20240314011521_create_admin_stocks.rb
+++ db/migrate/20240314011521_create_admin_stocks.rb
@@ -1,9 +1,9 @@
 class CreateAdminStocks < ActiveRecord::Migration[7.0]
   def change
-    create_table :admin_stocks do |t|
-      t.string :size
-      t.integer :amount
+    create_table :stocks do |t|
       t.references :product, null: false, foreign_key: true
+      t.integer :amount
+      t.string :size
 
       t.timestamps
     end
