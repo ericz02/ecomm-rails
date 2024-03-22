a5433d9eb8593b2f27465ddce256ed0005112509
ericz02 <ezheng0720@gmail.com>
2024-03-21 22:11:28 -0400

    feat: sets up home page, navbar, footer, added routes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Allow the asset pipeline to compile assets on the fly.
+  config.assets.debug = true
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
